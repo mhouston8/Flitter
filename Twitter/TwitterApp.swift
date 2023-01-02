713617300772904e513616f7ed109398f812af25
Matthew Houston <matthew.houston7@icloud.com>
2023-01-01 18:00:18 -0800

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Matthew Houston on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
